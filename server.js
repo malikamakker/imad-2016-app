8021e5e075c7fad653b996453f6d929b12714ced
malikamakker <malikamakker25@gmail.com>
2016-10-16 16:16:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-one',function(req,res){
 var counter=0;
 app.get('\counter',function(req,res){
     counter=counter+1;
-    req.send(counter.toString());
+    res.send(counter.toString());
 });
 
 app.get('/ui/style.css', function (req, res) {
